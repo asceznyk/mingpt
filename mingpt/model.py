46c13ea78d44a6fdcab0f4406b2f3f9b010dd405
asceznyk <asceznyk@gmail.com>
2021-07-22 19:22:17 +0530

    little by little

--- mingpt/model.py
+++ mingpt/model.py
@@ -22,7 +22,7 @@ class GPT1Config(GPTConfig):
     n_head = 12
     n_embd = 768
 
-class SelfAttention(nn.Module):
+class MHSelfAttention(nn.Module):
     def __init__(config):
         assert config.n_embd % config.n_head == 0
 
@@ -38,3 +38,24 @@ class SelfAttention(nn.Module):
         self.register_buffer('mask', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
 
         self.n_head = config.n_head
+
+
+    def forward(self, x):
+        B, S, E = x.size()
+
+        #k, q, v (B, nh, S, hs) 
+        k = self.key(x).view(B, S, -1, E // self.n_head).transpose(1,2)
+        q = self.query(x).view(B, S, -1, E // self.n_head).transpose(1,2)
+        v = self.value(x).view(B, S, -1, E // self.n_head).transpose(1,2)
+
+        #(B, nh S, hs) * (B, nh, hs, S) -> (B, nh, S, S)
+        attn = (q @ k.transpose(-2,-1) / math.sqrt(k.size(-1)))
+        attn = attn.masked_fill(self.mask[:,:,:S,:S] == 0, float('-inf'))
+        attn = F.softmax(attn, dim=-1)
+        attn = self.attn_drop(attn)
+        attn = attn @ v # (B, nh, S, S) * (B, nh, S, hs) -> (B, nh, S, hs)
+
+        y = attn.transpose(1,2).contiguous().view(B, S, E)
+
+        return self.resid_drop(self.proj(y))
+
