ec44e8286508e5328528212f51e847480273b129
asceznyk <asceznyk@gmail.com>
2021-07-24 18:03:32 +0530

    again testing

--- test.py
+++ test.py
@@ -10,9 +10,10 @@ from mingpt.utils import *
 
 set_seed(42)
 
-vocab_size = 30000
+seq_len = 5
+vocab_size = 60
 
-config = GPTConfig(vocab_size, 5, n_heads=12, n_layer=12, n_embd=768)
+config = GPTConfig(vocab_size, set_len, n_heads=12, n_layer=12, n_embd=768)
 gpt1 = GPT(config)
 
 idx = torch.tensor([[randint(0, vocab_size) for i in range(config.block_size)]])
