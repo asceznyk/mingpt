c1fc3af11da89e8e106a2d73076b1453e4c4e9f5
asceznyk <asceznyk@gmail.com>
2021-07-24 15:47:31 +0530

    101s

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ set_seed(42)
 config = GPTConfig(1000, 5, n_heads=12, n_layer=12, n_embd=768)
 gpt1 = GPT(config)
 
-idx = [randint(0, 1000) for i in range(config.block_size)]
+idx = torch.tensor([[randint(0, 1000) for i in range(config.block_size)]])
 y = gpt1(idx)
 
 print(y, y.size())
