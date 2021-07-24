9aa1791fa3314e3244a86f8bfa88ec7e11544e8c
asceznyk <asceznyk@gmail.com>
2021-07-24 18:01:53 +0530

    again testing

--- test.py
+++ test.py
@@ -10,11 +10,13 @@ from mingpt.utils import *
 
 set_seed(42)
 
-config = GPTConfig(1000, 5, n_heads=12, n_layer=12, n_embd=768)
+vocab_size = 30000
+
+config = GPTConfig(vocab_size, 5, n_heads=12, n_layer=12, n_embd=768)
 gpt1 = GPT(config)
 
-idx = torch.tensor([[randint(0, 1000) for i in range(config.block_size)]])
-trg = torch.tensor([[randint(0, 1000) for i in range(config.block_size)]])
+idx = torch.tensor([[randint(0, vocab_size) for i in range(config.block_size)]])
+trg = torch.tensor([[randint(0, vocab_size) for i in range(config.block_size)]])
 
 print(idx.size(), idx)
 
