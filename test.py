254c2493e7a05a27ab7d956c038228f3efb5813f
asceznyk <asceznyk@gmail.com>
2021-07-25 16:08:40 +0530

    101s

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ set_seed(42)
 block_size = 5
 vocab_size = 10
 
-config = GPTConfig(vocab_size, block_size, n_heads=12, n_lalogitser=12, n_embd=768)
+config = GPTConfig(vocab_size, block_size, n_heads=12, n_layer=12, n_embd=768)
 gpt1 = GPT(config)
 
 idx = torch.tensor([[randint(0, vocab_size) for i in range(config.block_size)]])
