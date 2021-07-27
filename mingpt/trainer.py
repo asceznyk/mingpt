ced92e9611397e47b44f60e278ba7a795e057f99
asceznyk <asceznyk@gmail.com>
2021-07-27 21:17:46 +0530

    enough for the day (not really!)

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -83,6 +83,12 @@ class Trainer:
                     optimizer.step()
 
                     if config.lr_decay:
+                        self.tokens = (y >= 0).sum()
+                        if self.tokens < self.warmup_tokens:
+                            lr_coeff = self.tokens / max(1, self.warmup_tokens)
+                        else:
+                            p = (self.tokens - self.warmup_tokens) / max(1, (self.final_tokens - self.warmup_tokens))
+                            lr_coeff = max(0.1, 0.5 * (1 + math.cos(p * math.pi)))
                         lr = config.learning_rate * lr_coeff
                         for pg in optimizer.param_groups:
                             pg['lr'] = lr
