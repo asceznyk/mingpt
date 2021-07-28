c85e943fac4eb0d2a2567acc98d52af437ade456
asceznyk <asceznyk@gmail.com>
2021-07-28 22:32:09 +0530

    sampling file added with sys.args

---
+++ sampletxt.py
@@ -0,0 +1,25 @@
+import sys
+import math
+
+import numpy as np
+
+from tqdm import tqdm
+
+import torch
+import torch.optim as optim
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data.dataloader import DataLoader
+
+device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
+test_dataset = CharDataset(open(sys.argv[1], 'r').read(), 128)
+
+mcfg = GPTConfig(test_dataset.vocab_size, test_dataset.block_size, n_layer=8, n_heads=8, n_embd=512)
+model = GPT(mcfg)
+model.load_state_dict(torch.load(sys.argv[2]))
+model = model.to(device)
+
+context = 'O God, O God!'
+x = torch.tensor([test_dataset.stoi[s] for s in context])[None, :].to(device)
+y = sample(model, x, 2500, temp=1.0, top_k=10)[0]
+completion = ''.join([test_dataset.itos[i] for i in y])
+
