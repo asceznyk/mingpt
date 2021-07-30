126ac043730eed3a566ad589d979127b0fbf38fd
asceznyk <asceznyk@gmail.com>
2021-07-30 21:14:58 +0530

    argparse!

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -53,6 +53,8 @@ class Trainer:
         raw_model = self.model.module if hasattr(self.model, "module") else model
         optimizer = raw_model.configure_optimizers(config)
 
+        print(self.ckpt_path)
+
         def run_epoch(split):
             is_train = split == 'train'
             model.train(is_train)
